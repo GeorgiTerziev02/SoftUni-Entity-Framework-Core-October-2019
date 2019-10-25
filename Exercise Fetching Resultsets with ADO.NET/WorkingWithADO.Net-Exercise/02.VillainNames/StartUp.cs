8993418d7f110cb58149f08daddf0c87f7c9959f
Georgi Terziev <terzata2002@abv.bg>
2019-10-25 17:39:37 +0300

    Exercise 1-3 done

---
+++ Exercise Fetching Resultsets with ADO.NET/WorkingWithADO.Net-Exercise/02.VillainNames/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _02.VillainNames
+{
+    class StartUp
+    {
+        private static string connectionString =
+            "Server=.\\SQLEXPRESS;" +
+            "Database=MinionsDb;" +
+            "Integrated Security=true;";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            connection.Open();
+
+            using (connection)
+            {
+                string queryText = @"  SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                       FROM Villains AS v 
+                                       JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                       GROUP BY v.Id, v.Name 
+                                       HAVING COUNT(mv.VillainId) > 3 
+                                       ORDER BY COUNT(mv.VillainId)";
+
+                SqlCommand command = new SqlCommand(queryText, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Name"]} - {reader["MinionsCount"]}");
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ Exercise Fetching Resultsets with ADO.NET/WorkingWithADO.Net-Exercise/03.MinionNames/StartUp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _03.MinionNames
+{
+    class StartUp
+    {
+        private static string connectionString =
+            "Server=.\\SQLEXPRESS;" +
+            "Database=MinionsDb;" +
+            "Integrated Security=true;";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            connection.Open();
+
+            int inputId = int.Parse(Console.ReadLine());
+
+            using (connection)
+            {
+                string queryText = $"SELECT Name FROM Villains WHERE Id = {inputId}";
+
+                SqlCommand command = new SqlCommand(queryText, connection);
+
+                string villainName = (string)command.ExecuteScalar();
+
+                if (villainName == null)
+                {
+                    Console.WriteLine($"No villain with ID {inputId} exists in the database.");
+                    return;
+                }
+
+                queryText = @$"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,
+                                         m.Name, 
+                                         m.Age
+                                    FROM MinionsVillains AS mv
+                                    JOIN Minions As m ON mv.MinionId = m.Id
+                                   WHERE mv.VillainId = {inputId}
+                                ORDER BY m.Name";
+                command = new SqlCommand(queryText, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                Console.WriteLine($"Villain: {villainName}");
+                using (reader)
+                {
+                    if (reader.HasRows == false)
+                    {
+                        Console.WriteLine("(no minions)");
+                        return;
+                    }
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["RowNum"]}. {reader["Name"]} {reader["Age"]}");
+                    }
+                }
+            }
+        }
+    }
+}
