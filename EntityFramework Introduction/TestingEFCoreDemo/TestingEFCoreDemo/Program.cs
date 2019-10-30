1907a523c908562702f6ac6de1ae9222f5c0502a
Georgi Terziev <terzata2002@abv.bg>
2019-10-30 21:16:10 +0200

    done

---
+++ EntityFramework Introduction/TestingEFCoreDemo/TestingEFCoreDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using TestingEFCoreDemo.Data;
+
+namespace TestingEFCoreDemo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using (var db =new SoftUniContext())
+            {
+                var towns = db.Towns.ToList();
+
+                Console.WriteLine(towns.Count);
+            }
+        }
+    }
+}
