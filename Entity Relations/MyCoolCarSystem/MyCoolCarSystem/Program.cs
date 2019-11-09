a5188c22740fcfbd6c804490b186ca19577d6cc7
Georgi Terziev <terzata2002@abv.bg>
2019-11-09 19:30:02 +0200

    Lab done

--- Entity Relations/MyCoolCarSystem/MyCoolCarSystem/Program.cs
+++ Entity Relations/MyCoolCarSystem/MyCoolCarSystem/Program.cs
@@ -3,7 +3,10 @@
     using Microsoft.EntityFrameworkCore;
     using MyCoolCarSystem.Data;
     using MyCoolCarSystem.Data.Models;
+    using MyCoolCarSystem.Results;
     using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
 
     public class Program
@@ -15,25 +18,42 @@
             db.Database.Migrate();
 
             var result = db.Purchases
-                .Select(p => new
+                .Select(p => new PurchaseResultModel
                 {
-                    p.Price,
-                    p.PurchaseDate,
-                    Customer = new
+                    Price = p.Price,
+                    PurchaseDate = p.PurchaseDate,
+                    Customer = new CustomerResultModel
                     {
                         Name = p.Customer.FirstName + " " + p.Customer.LastName,
-                        p.Customer.Address.Town
+                        Town = p.Customer.Address.Town
                     },
 
-                    Car = new
+                    Car = new CarResultModel
                     {
                         Make = p.Car.Model.Make.Name,
                         Model = p.Car.Model.Name,
-                        p.Car.Vin
+                        Vin = p.Car.Vin
                     }
                 })
-                .ToList(); 
-            
+                .ToList();
+
+            var make = db.Makes.FirstOrDefault(m=>m.Name=="Mercedes");
+
+            var model = new Model
+            {
+                Modification = "500",
+                Name = "CL",
+                Year = 3000
+            };
+
+            var validationContext = new ValidationContext(model);
+            var validationResults = new List<ValidationResult>();
+
+            Validator.TryValidateObject(model, validationContext, validationResults, true);
+            Validator.ValidateObject(model, validationContext, true);
+
+            make.Models.Add(model);
+
             db.SaveChanges();
         }
     }
---
+++ Entity Relations/MyCoolCarSystem/MyCoolCarSystem/Results/PurchaseResultModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MyCoolCarSystem.Results
+{
+    using System;
+
+    public class PurchaseResultModel
+    {
+        public decimal Price { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+        
+        public CarResultModel Car { get; set; }
+
+        public CustomerResultModel Customer { get; set; }
+    }
+}
