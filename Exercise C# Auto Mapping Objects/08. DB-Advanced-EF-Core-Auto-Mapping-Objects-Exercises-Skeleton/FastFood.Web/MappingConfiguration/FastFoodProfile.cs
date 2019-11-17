b6ec375fcb1ee3a5f6b37fabcb6faa30d536c917
Georgi Terziev <terzata2002@abv.bg>
2019-11-17 21:52:18 +0200

    done

---
+++ Exercise C# Auto Mapping Objects/08. DB-Advanced-EF-Core-Auto-Mapping-Objects-Exercises-Skeleton/FastFood.Web/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,58 @@
+﻿namespace FastFood.Web.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Web.ViewModels.Categories;
+    using FastFood.Web.ViewModels.Employees;
+    using FastFood.Web.ViewModels.Items;
+    using FastFood.Web.ViewModels.Orders;
+    using Models;
+
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x => x.PositionId, y => y.MapFrom(s => s.Id))
+                .ForMember(x => x.PositionName, y => y.MapFrom(x => x.Name));
+
+            //Employees
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.Position, y => y.MapFrom(x => x.Position.Name));
+
+            //Categories
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x => x.Name, y => y.MapFrom(x => x.CategoryName));
+
+            this.CreateMap<Category, CategoryAllViewModel>();
+
+            this.CreateMap<Category, CreateItemViewModel>()
+                .ForMember(x => x.CategoryId, y => y.MapFrom(x => x.Id))
+                .ForMember(x => x.CategoryName, y => y.MapFrom(x => x.Name));
+
+            //Items
+            this.CreateMap<CreateItemInputModel, Item>();
+
+            this.CreateMap<Item, ItemsAllViewModels>()
+                .ForMember(x => x.Category, y => y.MapFrom(x => x.Category.Name));
+
+
+            //Orders
+            this.CreateMap<CreateOrderInputModel, Order>();
+
+            this.CreateMap<Order, OrderAllViewModel>()
+                .ForMember(x => x.OrderId, y => y.MapFrom(s => s.Id))
+                .ForMember(x => x.Employee, y => y.MapFrom(s => s.Employee.Name));
+        }
+    }
+}
---
+++ Exercise C# Auto Mapping Objects/08. DB-Advanced-EF-Core-Auto-Mapping-Objects-Exercises-Skeleton/FastFood.Web/ViewModels/Orders/CreateOrderInputModel.cs
@@ -0,0 +1,19 @@
+﻿namespace FastFood.Web.ViewModels.Orders
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CreateOrderInputModel
+    {
+        [Required]
+        [MinLength(3), MaxLength(30)]
+        public string Customer { get; set; }
+
+        public int ItemId { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int Quantity { get; set; }
+    }
+}
