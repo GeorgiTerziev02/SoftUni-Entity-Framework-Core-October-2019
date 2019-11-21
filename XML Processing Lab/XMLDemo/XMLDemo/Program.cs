631372f4f5ca18413056af1277fe59eac1b117d2
Georgi Terziev <terzata2002@abv.bg>
2019-11-21 20:04:28 +0200

    lab

---
+++ XML Processing Lab/XMLDemo/XMLDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace XMLDemo
+{
+    using System;
+    using System.IO;
+    using System.Xml.Linq;
+    using XMLDemo.Models;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument document = XDocument.Load("Data/cars.xml");
+            //document.Declaration.Version = "3.0";
+            //document.Root.Add(new XElement("car", new XElement("make", "Audi")));
+            //document.Save("Data/cars_updated.xml");
+
+            XElement root = document.Root;
+            var cars = root.Elements();
+
+            foreach (var car in cars)
+            {
+                var carObject = new Car()
+                {
+                    Make = car.Element("make").Value,
+                    Model = car.Element("model").Value,
+                    TravelledDistance = long.Parse(car.Element("travelled-distance").ToString())
+                };
+
+                Console.WriteLine($"{car.Element("make")} {car.Element("model").Value}");
+            }
+        }
+    }
+}
